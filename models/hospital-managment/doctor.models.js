297f933463dfe4de7c46945ba347f4c585b2512b
Rakheeb <rakheebshaikh906@gmail.com>
2026-08-29 01:15:57 +0530

    hospital-managment

---
+++ models/hospital-managment/doctor.models.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const doctorSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        lowercase: true
+    },
+    age: {
+        type: Number,
+        required: true,
+        lowercase: true
+    },
+    qulification: {
+        type: String,
+        required: true,
+        lowercase: true
+    },
+    previousHospital: {
+        type: String,
+        required: true,
+        lowercase: true
+    },
+    specialization: {
+        type: String,
+        required: true,
+        lowercase: true
+    },
+    experience: {
+        type: String,
+        required: true,
+        lowercase: true
+    },
+    fees: {
+        type: String,
+        required: true,
+        lowercase: true
+    }
+
+}, { timestamps: true });
+
+export const Doctor = mongoose.model('Doctor', doctorSchema);
