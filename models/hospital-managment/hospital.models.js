dafd444c53218142d767fb596e53a47e3d2ae6dc
Rakheeb <rakheebshaikh906@gmail.com>
2026-09-22 00:22:20 +0530

    addd functionality

--- models/hospital-managment/hospital.models.js
+++ models/hospital-managment/hospital.models.js
@@ -15,6 +15,10 @@ const hospital = new mongoose.Schema({
         type: String,
         lowercase: true
     },
+    address: {
+        type: String,
+        lowercase: true
+    },
     doctors: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Doctor'
